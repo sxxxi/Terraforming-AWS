25f4a0858f95fd1b83c0f0048b3ba0bee8daed5a
Seiji Akakabe <akakabeseiji0@gmail.com>
2023-11-21 11:05:00 -0500

    update scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Login and build
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $1
+aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin $1
 
-docker build -t l3-cats-repo cats
-docker build -t l3-dogs-repo dogs
+sudo docker build -t l3-cats-repo cats
+sudo docker build -t l3-dogs-repo dogs
 
-docker tag l3-cats-repo:latest $1/l3-cats-repo:latest
-docker tag l3-dogs-repo:latest $1/l3-dogs-repo:latest
+sudo docker tag l3-cats-repo:latest $1/l3-cats-repo:latest
+sudo docker tag l3-dogs-repo:latest $1/l3-dogs-repo:latest
 
-docker push $1/l3-cats-repo:latest
-docker push $1/l3-dogs-repo:latest
+sudo docker push $1/l3-cats-repo:latest
+sudo docker push $1/l3-dogs-repo:latest
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,17 +15,17 @@ fi
 cd ~
 
 # Login and pull
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $1
+aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin $1
 
 cats=$1/l3-cats-repo:latest
 dogs=$1/l3-dogs-repo:latest
 
-docker pull $cats
-docker pull $dogs
+sudo docker pull $cats
+sudo docker pull $dogs
 
 # Deploy
-docker run -dp 8081:80 $dogs
-docker run -dp 8080:80 $cats
+sudo docker run -dp 8081:80 $dogs
+sudo docker run -dp 8080:80 $cats
 
 
 
